dc74be5203488e5eb5a1d233ebad8af12c69854b
guan.kai <guankaigeren@163.com>
2020-12-01 16:29:17 +0800

    修改

--- src/main/java/com/demo/springbootdemo/config/WebConfigurer.java
+++ src/main/java/com/demo/springbootdemo/config/WebConfigurer.java
@@ -7,6 +7,10 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * web拦截器配置类
  *
@@ -20,21 +24,21 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
 
-    /** 放行接口路径 */
-    private static final String[] EXCLUDE_PATHS = {
-            "/test",
-            "/login",
-            "/register"
-    };
 
-    /** 静态资源路径 */
-    private static final String[] STATIC_RESOURCES = {
-            "/css/**",
-            "/js/**",
-            "/images/**",
-            "/fonts/**",
-            "/bootstrap/**"
-    };
+    /** 放行路径 */
+    private static final List<String> EXCLUDE_PATHS = new ArrayList<>();
+
+    static {
+        /** 静态资源路径 */
+        EXCLUDE_PATHS.addAll(Arrays.asList("/css/**", "/js/**", "/images/**", "/fonts/**", "/bootstrap/**"));
+
+        /** 接口路径 */
+        EXCLUDE_PATHS.add("/test");
+        EXCLUDE_PATHS.add("/test/post");
+        EXCLUDE_PATHS.add("/login");
+        EXCLUDE_PATHS.add("/register");
+    }
+
 
 
     /**
@@ -48,8 +52,7 @@ public class WebConfigurer implements WebMvcConfigurer {
         // excludePathPatterns("/login", "/register") 表示除了登陆与注册之外，因为登陆注册不需要登陆也可以访问
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns(EXCLUDE_PATHS)
-                .excludePathPatterns(STATIC_RESOURCES);
+                .excludePathPatterns(EXCLUDE_PATHS);
 //        super.addInterceptors(registry);    //较新Spring Boot的版本中这里可以直接去掉，否则会报错
     }
 
@@ -62,7 +65,8 @@ public class WebConfigurer implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler(this.url + "**")
 //                .addResourceLocations("file:" + this.path);
-        //addResourceHandler("/static/**")     访问路径
-        //addResourceLocations("classpath:/static/")    资源真实路径
+        //addResourceHandler("/static/**")     //对外访问路径
+        //addResourceLocations("classpath:/static/")    //资源真实路径
+        //addResourceLocations("file:/work/")    //资源真实路径
     }
 }
